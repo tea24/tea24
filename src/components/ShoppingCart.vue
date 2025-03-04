eaeacc97adf40942e3ca40d2d4d01dd02668f591
tea24 <amnecoin@gmail.com>
2025-03-05 03:51:39 +0700

    add adding and removing functionality to shopping cart

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="shopping-cart">
+    <div class="shopping-cart-widget" :class="{'active': items.length == 0}">Корзина</div>
+    <div class="shopping-cart" :class="{'active': items.length > 0}">
         <div class="shopping-cart-header">Корзина</div>
         <div class="shopping-cart-content">
             <div class="shopping-cart-item" v-for="item in items" :key="item">
-                <h5 class="item-heading">{{ item.name }}</h5>
                 <img :src="require('../items/'+item.image)">
+                <h5 class="item-heading">{{ item.name }}</h5>
+                <button @click="store.cart.splice(store.cart.indexOf(item), 1)">Удалить</button>
             </div>
         </div>
     </div>
@@ -15,23 +17,53 @@ import { defineComponent } from 'vue';
 import { store } from '../store.js'
 
 export default defineComponent({
+    data() {
+        return {
+            
+        }
+    },
     setup() {
         return {
+            store,
             items: store.cart
         }
+    },
+    methods: {
+        
     }
 })
 </script>
 
 <style scoped>
-.shopping-cart {
+.active {
+    display: block !important;
+}
+.shopping-cart-widget {
+    background-color: #fff;
+    width: 100px;
+    display: none;
     position: fixed;
     bottom: 0;
     right: 0;
+}
+.shopping-cart {
+    float: left;
+    display: none;
+    position: fixed;
+    overflow-y: scroll;
+    bottom: 0;
+    right: 0;
     width: 300px;
-    height: calc(100% - 4rem);
+    height: calc(100% - 11rem);
     background-color: #fff;
     padding: 1rem;
     margin: 1rem;
+}
+.shopping-cart-item {
+    float: left;
+}
+.shopping-cart img {
+    width: 100px;
+    height: 100px;
 }
 </style>
