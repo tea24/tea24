af0b51dbda9c0ee103cd4409f27c74669d050a5e
tea24 <amnecoin@gmail.com>
2025-03-07 02:56:01 +0700

    show and hide shopping cart

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="shopping-cart-widget" :class="{'active': items.length == 0}">Корзина</div>
-    <div class="shopping-cart" :class="{'active': items.length > 0}">
-        <div class="shopping-cart-header">Корзина</div>
+    <button class="shopping-cart-widget" v-if="items.length == 0 || !displayCart" @click="displayCart = true">Корзина</button>
+    <div class="shopping-cart" v-if="items.length > 0 && displayCart">
+        <div class="shopping-cart-header">Корзина <button @click="displayCart = false">_</button></div>
         <div class="shopping-cart-content">
             <div class="shopping-cart-item" v-for="item in items" :key="item">
                 <img :src="require('../items/'+item.image)">
@@ -19,7 +19,7 @@ import { store } from '../store.js'
 export default defineComponent({
     data() {
         return {
-            
+            displayCart: false
         }
     },
     setup() {
@@ -35,20 +35,15 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.active {
-    display: block !important;
-}
 .shopping-cart-widget {
     background-color: #fff;
     width: 100px;
-    display: none;
     position: fixed;
     bottom: 0;
     right: 0;
 }
 .shopping-cart {
     float: left;
-    display: none;
     position: fixed;
     overflow-y: scroll;
     bottom: 0;
