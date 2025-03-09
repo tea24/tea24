8fbfc3c108130282bdb7852553b243d80f2d85c8
tea24 <amnecoin@gmail.com>
2025-03-10 00:47:56 +0700

    send order data to telegram

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -3,6 +3,7 @@
     <div class="shopping-cart" v-if="page == Pages.CART">
         <div class="shopping-cart-header">Корзина ({{ items.reduce((acc, x) => acc + x.price, 0) }}) руб <button @click="page = Pages.NONE">_</button></div>
         <div class="shopping-cart-content" v-if="items.length > 0">
+            <button class="button-primary" @click="page = Pages.ORDER">Оформить заказ</button>
             <div class="shopping-cart-item" v-for="item in items" :key="item">
                 <img :src="require('../items/'+item.image)">
                 <h5 class="item-heading">{{ item.name }}</h5>
@@ -13,11 +14,32 @@
             <h5>Нет товаров в корзине</h5>
         </div>
     </div>
+    <div class="shopping-cart" v-if="page == Pages.ORDER">
+        <div class="shopping-cart-header">Корзина ({{ items.reduce((acc, x) => acc + x.price, 0) }}) руб <button @click="page = Pages.NONE">_</button></div>
+        <div class="shopping-cart-content" v-if="items.length > 0">
+            <button class="button-primary" @click="page = Pages.CART">Назад в корзину</button>
+            <form>
+                <div class="buttons">
+                    <input type="radio" name="contact" value="whatsapp" id="whatsapp" checked v-model="form.contact"><label for="whatsapp">Whatsapp</label>
+                    <input type="radio" name="contact" value="telegram" id="telegram" v-model="form.contact"><label for="telegram">Telegram</label>
+                    <input type="radio" name="contact" value="phone" id="phone" v-model="form.contact"><label for="phone">Телефон</label>
+                </div>
+                
+                <input type="text" placeholder="@nickname или +79996662010" v-model="form.phone">
+                <button @click="sendOrder">Send</button>
+            </form>
+            <p>{{ form.phone }}</p>
+        </div>
+        <div class="shopping-cart-content" v-else>
+            <h5>Нет товаров в корзине</h5>
+        </div>
+    </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import { store } from '../store.js'
+import axios from 'axios'
 
 const Pages = {
     NONE: 0,
@@ -29,7 +51,13 @@ export default defineComponent({
     data() {
         return {
             page: Pages.NONE,
-            Pages
+            Pages,
+            form: {
+                phone: null,
+                contact: "whatsapp",
+            },
+            token: "7043531371:AAESezj7TB8HwxvkFDVNFlJqGRYvctgQRcg",
+            chat_ids: [1634734, 7423200602],
         }
     },
     setup() {
@@ -39,7 +67,23 @@ export default defineComponent({
         }
     },
     methods: {
-        
+        async sendOrder(event) {
+            event.preventDefault()
+            if (this.form.phone !== "") {
+                this.chat_ids.forEach(async id => {
+                    const send_message_url = `https://api.telegram.org/bot${this.token}/sendMessage`
+                    const data = {
+                        'text': `${this.form.contact} ${this.form.phone} ${JSON.stringify(store.cart)}`,
+                        'chat_id': id,
+                        'parse_mode': 'markdown',
+                    }
+                    const response = await axios.post(send_message_url, data)
+                    console.log(response) 
+                });
+            } else {
+                console.log("NOT PHONE", this.form.phone)
+            }
+        }
     }
 })
 </script>
@@ -71,6 +115,12 @@ export default defineComponent({
 .shopping-cart img {
     width: 100px;
     height: 100px;
+}
+.buttons {
+    display: ruby;
+}
+.buttons label {
+    margin-right: 1rem;
 }
 
 /* Smaller than 550 */
